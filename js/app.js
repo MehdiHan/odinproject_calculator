2ef7338e0b542d8ee77b72e8896c7e6f5860d39d
MehdiHan <meh.bouchard@outlook.fr>
2020-10-11 07:33:53 +0200

    Adding eventLister on equal, returnNumbers function and operate logic

--- js/app.js
+++ js/app.js
@@ -1,51 +1,58 @@
 const buttons = document.querySelectorAll('[data-value]');
+const equal = document.querySelector('[data-key=equal]');
 const displayResult = document.getElementById('displayResult');
-
-/**
- * Returns the summ of a and b
- * @param {Number} a integer or float, positive or negative
- * @param {Number} b integer or float, positive or negative
- */
-
-const add = (a, b) => {
-  return a + b;
-};
-
-/**
- * Returns the subtraction of b from a
- * @param {Number} a integer or float, positive or negative
- * @param {Number} b integer or float, positive or negative
- */
-const subtract = (a, b) => {
-  return a - b;
-};
-
-/**
- * Returns the multiplication of a by b
- * @param {Number} a integer or float, positive or negative
- * @param {Number} b integer or float, positive or negative
- */
-const multiply = (a, b) => {
-  return a * b;
-};
-
-/**
- * Returns the division of a by b
- * @param {Number} a integer or float, positive or negative
- * @param {Number} b integer or float, positive or negative
- */
-const divide = (a, b) => {
-  return a / b;
-};
 
 /**
  * Returns the operation to a and b
  * @param {Number} a integer or float, positive or negative
  * @param {Number} b integer or float, positive or negative
- * @param {function} operator function that take 2 numbers as arguments
+ * @param {string} operator string can be 'add','subtract','multiply' or 'divide'
  */
 const operate = (a, b, operator) => {
-  return operator(a, b);
+  if (operator === 'add') {
+    result = a + b;
+  }
+  if (operator === 'subtract') {
+    result = a - b;
+  }
+  if (operator === 'multiply') {
+    result = a * b;
+  }
+  if (operator === 'divide') {
+    result = a / b;
+  }
+  return result;
+};
+
+/**
+ *
+ * @param {string} string literal expression of a calculation
+ * return value of the string before math sign , value of the string after math sign , math sign himself
+ */
+const returnNumbers = (string) => {
+  let index = '';
+  let operator = '';
+  if (string.includes('+')) {
+    index = string.indexOf('+');
+    operator = 'add';
+  }
+  if (string.includes('-')) {
+    index = string.indexOf('-');
+    operator = 'subtract';
+  }
+  if (string.includes('*')) {
+    index = string.indexOf('*');
+    operator = 'multiply';
+  }
+  if (string.includes('/')) {
+    index = string.indexOf('/');
+    operator = 'divide';
+  }
+
+  let string1 = string.substr(0, index);
+  let string2 = string.substr(index + 1, string.length);
+
+  return [string1, string2, operator];
 };
 
 buttons.forEach((button) => {
@@ -70,4 +77,13 @@ buttons.forEach((button) => {
       }
     }
   });
+});
+
+equal.addEventListener('click', function (event) {
+  let string = returnNumbers(displayResult.innerText);
+  let a = Number.parseFloat(string[0], 10);
+  let b = Number.parseFloat(string[1], 10);
+  let operator = string[2];
+  let result = operate(a, b, operator);
+  displayResult.innerText = result;
 });
